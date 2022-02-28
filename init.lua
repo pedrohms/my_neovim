ee4c536382aa1d5988f8877c70fabf188448dbc8
Pedro Henrique <pedro@pedrohms.com>
2022-02-28 14:52:48 -0300

    Commit inicial

---
+++ init.lua
@@ -0,0 +1,33 @@
+require('user.plugins')
+require('user.cmp')
+require('user.options')
+require('user.keymaps')
+require('user.bufferline')
+require('user.project')
+require('user.comment')
+require('user.alpha')
+require('user.autopairs')
+require('user.toggleterm')
+require('user.treesitter')
+require('user.colorschema')
+require('user.lsp')
+require('user.telescope')
+require('user.whichkey')
+-- require('user.basics')
+-- require('user.colors')
+-- require('user.telescope-config')
+-- require('user.coc-config')
+-- require('user.lualine').setup()
+
+-- require'nvim-treesitter.configs'.setup {
+--   ensure_installed = "maintained",
+--   context_commentstring = {
+--     enable = true
+--   },
+--   highlight = {
+--     enable = true
+--   },
+--   indent = {
+--     enable = true
+--   }
+-- }
